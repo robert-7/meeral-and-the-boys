c279b7747fe3324b6bc233520282f3abae2f7ebe
robert-7 <robert.lech123@gmail.com>
2019-09-17 17:43:47 -0400

    small fixes

--- src/routes/plane.js
+++ src/routes/plane.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     return res.send(Object.values(req.context.models.master.planes));
   });
 
-router.put('/', (req, res) => {
+router.post('/', (req, res) => {
   const id = uuidv4();
 
   const newPlane = {
@@ -15,8 +15,8 @@ router.put('/', (req, res) => {
     lives: 20,
     coord: [ // (radius - [0-100], y-rotation - [0-359], z-rotation - [0-359])
         Math.floor(Math.random() * 100) + 1, 
-        Math.floor(Math.random() * 359) + 1,
-        Math.floor(Math.random() * 359) + 1
+        Math.floor(Math.random() * 360) + 1,
+        Math.floor(Math.random() * 360) + 1
     ],
     rotation: 90,
   };
@@ -25,18 +25,19 @@ router.put('/', (req, res) => {
   return res.send(newPlane);
 });
 
-router.post('/:planeID', (req, res) => {
+router.put('/:planeID', (req, res) => {
     var plane = req.context.models.master.planes[req.params.planeID];
+    console.log(plane);
     if (!!plane) {
         const planeUpdate = req.body
         for (var key in planeUpdate) { plane[key] = planeUpdate[key]; }
     }
     else{
-        console.log('plane not found')
+        console.log('plane not found');
         return res.send('plane not found');
     }
         
     return res.send(plane);
-  });
+});
 
 export default router;
