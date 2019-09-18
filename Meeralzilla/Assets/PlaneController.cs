3ba88723f1a611abcef7e364d8a69a76dc0e7272
Daniel Shanker <daniel.shanker@indexexchange.com>
2019-09-18 15:42:13 -0400

    Added max and min height

--- Meeralzilla/Assets/PlaneController.cs
+++ Meeralzilla/Assets/PlaneController.cs
@@ -13,6 +13,8 @@ public class PlaneController : MonoBehaviour
 
     private float SideSpeed = 1.0f;
     private float UpdownSpeed = 1.0f;
+    private float maxHeight = 40;
+    private float minHeight = 0;
 
     void Update()
     {
@@ -70,11 +72,17 @@ public class PlaneController : MonoBehaviour
     }
 
     public void GoUp() {
-        tiltAroundZ = tiltAroundZ + UpdownSpeed;
+        if (transform.eulerAngles.z < maxHeight)
+        {
+            tiltAroundZ = tiltAroundZ + UpdownSpeed;
+        }
     }
 
     public void GoDown() {
-        tiltAroundZ = tiltAroundZ - UpdownSpeed;
+        if (transform.eulerAngles.z > minHeight)
+        {
+            tiltAroundZ = tiltAroundZ - UpdownSpeed;
+        }
     }
 
 
