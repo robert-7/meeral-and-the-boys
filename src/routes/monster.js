78b5b63df20ce8ae515d37c0f4c4faed70b63bad
Jacky <jchowca@yahoo.ca>
2019-09-18 15:57:45 -0400

    Remove monster useless code

--- src/routes/monster.js
+++ src/routes/monster.js
@@ -30,38 +30,4 @@ router.post('/', (req, res) => {
 
 });
 
-router.put('/', (req, res) => {
-    var monster = req.context.models.master.monster;
-    if (!!monster) {
-        const monsterUpdate = req.body
-        for (var key in monsterUpdate) { monster[key] = monsterUpdate[key]; }
-        // when a collision event occurs, decrement the lives of the planeIDCollifed
-        if (!!monsterUpdate["event"] && monsterUpdate["event"]["type"] == "collision") {
-            const planeID = monsterUpdate["event"]["planeID"];
-            const plane = req.context.models.master.planes[planeID];
-            if (!!plane) {
-                plane["lives"]--;
-                // handle plane death
-                if (plane["lives"] === 0) {
-                    delete req.context.models.master.planes[planeID];
-                }
-                else {
-                    plane["planeDeath"] = Date.now();
-                    plane["status"] = "dead";
-                }
-            }
-            else {
-                console.log('plane not found');
-                return res.send('plane not found');
-            }
-        }
-    }
-    else{
-        console.log('monster not found')
-        return res.send('monster not found');
-    }
-        
-    return res.send(req.context.models.master);
-  });
-
 export default router;
