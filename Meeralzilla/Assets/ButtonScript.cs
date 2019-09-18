dace6520fc7f8703aaac86a17f83c6de719765a1
Daniel Shanker <daniel.shanker@indexexchange.com>
2019-09-18 10:32:38 -0400

    Added Plane UI

---
+++ Meeralzilla/Assets/ButtonScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    bool goLeft;
+    bool goRight;
+    bool goUp;
+    bool goDown;
+    GameObject plane;
+
+
+    void Start ()
+    {
+        plane = GameObject.Find("PlaneBase");
+    }
+
+    void Update ()
+    {
+        if (goLeft == true) {
+            plane.GetComponent<PlaneController>().GoLeft();
+        }
+        if (goRight == true)
+        {
+            plane.GetComponent<PlaneController>().GoRight();
+        }
+        if (goUp == true)
+        {
+            plane.GetComponent<PlaneController>().GoUp();
+        }
+        if (goDown == true)
+        {
+            plane.GetComponent<PlaneController>().GoDown();
+        }
+        Debug.Log("test");
+    }
+
+    public void leftDown ()
+    {
+        goLeft = true;
+    }
+
+    public void leftUp ()
+    {
+        goLeft = false;
+    }
+
+    public void upDown()
+    {
+        goUp = true;
+    }
+
+    public void upUp()
+    {
+        goUp = false;
+    }
+
+    public void downDown()
+    {
+        goDown = true;
+    }
+
+    public void downUp()
+    {
+        goDown = false;
+    }
+
+    public void rightDown()
+    {
+        goRight = true;
+    }
+
+    public void rightUp()
+    {
+        goRight = false;
+    }
+
+}
