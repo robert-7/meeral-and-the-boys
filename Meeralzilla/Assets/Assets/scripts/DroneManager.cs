965a799e6fef8b3931155538e94e14324852a019
Adam Bysice <adambysice@gmail.com>
2019-09-18 11:22:49 -0400

    added plane prefab

---
+++ Meeralzilla/Assets/Assets/scripts/DroneManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneManager : MonoBehaviour
+{
+    public GameObject dronePrefab;
+
+    private Dictionary<string, GameObject> droneMap;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        droneMap = new Dictionary<string, GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateState(GameState gs) {
+
+    }
+
+    private void CreateDrone(string s, Vector3 rotation) {
+        if (!droneMap.ContainsKey(s))
+        {
+            GameObject newPlane = Instantiate(dronePrefab, new Vector3(), Quaternion.Euler(0, rotation.y, rotation.z));
+            droneMap.Add(s, newPlane);
+        }
+    } 
+
+    private void RemoveDrone(string s)
+    {
+        if (droneMap.ContainsKey(s))
+        {
+            GameObject doomedPlane = droneMap[s];
+            GameObject.Destroy(doomedPlane);
+            droneMap.Remove(s);
+        }
+    }
+
+}
